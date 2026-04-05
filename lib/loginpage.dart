fa7a26d256cceb9c744537214f3f174f1d36f124
DPerrotta06 <danielperrotta12066@gmail.com>
2026-04-04 23:25:59 -0400

    Connected firebase and cleaned code and file structure

---
+++ lib/loginpage.dart
@@ -0,0 +1,168 @@
+import 'package:balmart/registration.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [
+                Colors.white,
+                Colors.lightBlueAccent,
+                Colors.lightGreenAccent,
+                Colors.orangeAccent,
+              ],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Bal',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'mart',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    TextField(
+                      textAlign: TextAlign.center,
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.blueAccent,
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(180),
+                          borderSide: BorderSide(
+                            color: Colors.white70,
+                            width: 3.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(180),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 3.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    TextField(
+                      textAlign: TextAlign.center,
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.blueAccent,
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(180),
+                          borderSide: BorderSide(
+                            color: Colors.white70,
+                            width: 3.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(180),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 3.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        elevation: 10,
+                        shadowColor: Colors.deepOrange,
+                        fixedSize: Size(150, 50),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    TextButton(
+                      onPressed: () {
+                        if (mounted) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (_) => Registration()),
+                          );
+                        }
+                      },
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Don't have an account?\nCreate one today!",
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Poppins',
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
